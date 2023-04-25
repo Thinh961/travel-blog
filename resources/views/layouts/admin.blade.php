c00f594ac8b2b99773512fab7d5c2af9cd58d4b4
Komachi21 <nguyenduchieutt76@gmail.com>
2023-04-25 21:29:40 +0700

    init project laravel

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin-frame')
+@section('admin-layout')
+    @include('admin.components.sidebar')
+
+    <div class="content">
+        @include('admin.components.navbar')
+        <div class="container-fluid pt-4 px-4">
+            @yield('content')
+        </div>
+
+        @include('admin.components.footer')
+    </div>
+@endsection
